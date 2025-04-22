664b74bff1735f367134627a9495567f6470ae4c
Michael McKeehan <mmckeeha@gmail.com>
2025-04-22 09:12:53 -0400

    project 1 and readme commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# My journey learning Python
+
+## About
+
+This public repo will hold the different projects I have done over the course of my learning journey. The end goal is to gain the skills to be able to either enhance my abilities as a IAM analyst/ engineer, or to move into the Python development space. I will include a brief description of projects as I complete them.
+
+## Projects
+
+### Project 1: Simple_Fight.py
+
+This is a simple "fighting game" to learn the very basics of classes in Python.
+
+The program starts with defining the enemy class. This will set the basics of health and shields as well as contain a function to destroy the enemy.
+
+The "main" function is the core logic of the program. This is where the player will iterate through fights with enemies. Right now there are only two enemies.
+
+The "fight" function is where the player will fight the enemy. This seems unfair since the enemy will not fight back (this will be implemented in a later iteration of the game). The player will do a random amount of damage (between 1 and 4). 
---
+++ Simple_Fight.py
@@ -0,0 +1,45 @@
+"""
+This is a simple "fighting" game that was built to test out using a simple class. While this is a simple script, I was able to learn
+alot about the flow of a program and simple debugging. 
+"""
+
+import random
+
+class Enemy:
+    def __init__(self, health, shield_range):
+        self.health = health
+        self.shield = random.randint(shield_range[0], shield_range[1])
+    def destroy_enemy(self):
+        print("Enemy Destroyed")
+
+def main():
+    player_attack = (1, 4)
+    enemy1 = Enemy(5, (10, 16))
+    enemy2 = Enemy(5, (20, 51))
+    print("Wild enemy appears\n")
+    fight(enemy1, player_attack)
+    print ("You win!\n\n...\n\nWait, what's that?\n")
+
+    fight(enemy2, player_attack)
+    print("Now you win!") 
+
+def fight(enemy, attack):
+    shield = enemy.shield
+    health = enemy.health
+    while health > 0:
+        print(f"   Enemy Health: {health}\n   Enemy Shield: {shield}\n")
+        input("Press \"Enter\" to attack\n")
+        damage = random.randint(attack[0], attack[1])
+        print(f"Attack Damage: {damage}\n")
+        if shield > 0:
+            if damage >= shield:
+                shield = 0
+            else:
+                shield -= damage
+        else:
+            health -= damage
+    if health <= 0:
+        Enemy.destroy_enemy(enemy)
+
+if __name__ == "__main__":
+    main()
